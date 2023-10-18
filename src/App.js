60ca04cbded3eee368e7a22119ae339fead26656
Ravi Prakash <workxravi@gmail.com>
2023-10-18 11:42:20 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{height:'100vh', width:'100vw', display:'flex', justifyContent:'center', alignItems:'center', flexDirection:'column', gap:'24px'}}>
+      <div style={{fontSize:'24px'}}>Welcome to <span style={{fontWeight:'600'}}>CodeTapestry</span>.</div>
+      <div style={{fontSize:'18px'}}>Site under development!!</div>
     </div>
   );
 }
